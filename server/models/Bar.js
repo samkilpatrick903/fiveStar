a3bd5ccba43517b0436b61ead76b98be1ee95ad4
brooke <brookejones406@gmail.com>
2021-12-02 17:32:32 -0600

    db

---
+++ server/models/Bar.js
@@ -0,0 +1,52 @@
+
+{
+    "id": 456
+    "name": "Maudie's",
+    "type": "(bar or restaurant)"
+    "location": "Street address"
+    "drinks": [
+        {
+            "drink_name": "Mexican Martini",
+            "drink_type": mixed
+        },
+        {
+            "drink_name": "Frozen margarita",
+            "drink_type": mixed
+        },
+        {
+            "drink_name": "Frozen margarita",
+            "drink_type": mixed
+        },
+        
+    "users": [{
+        "user_id": 1,
+        "user_id": 2,
+    }]
+};
+
+//to add: 
+// Lala's
+// Wonderbar
+// Taco Flats
+// Workhorse Bar
+// Dogwood
+// Contigo 
+// The Roosevelt Room
+// Cheer Up Charlie's
+// Kung Fu Saloon
+// Placeholder
+// The Little Darlin'
+//Chuy's 
+// Lustre Pearl
+// Deep Eddy Cabaret
+// The White Horse
+// Cheer Up Charlie's
+// Yellow Jacket Social Club
+// Armadillo Den
+// Zanzibar
+// Easy Tiger
+// Kitty Cohen's
+// Kinda Tropical
+// Hotel San José
+// Frazier's
+// Haymaker
