8e3825f75522904a6d7502cf312381ba52030a20
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-12-02 19:32:19 -0600

    adding stuff

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,75 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+
+export const ADD_DATA=gql`
+mutation addData($coinData:newCoin){
+  addData(coinData:$coinData){
+    name
+
+  
+  }
+}
+
+`
+
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
+      token
+      user {
+        _id 
+        name
+        email
+       
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($newBook: InputBook!) {
+    saveBook(newBook: $newBook) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
