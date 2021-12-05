a0231ecc52831c9bbdd29a995a5a028276954a1a
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-12-05 17:57:57 -0600

    typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,40 +1,17 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type User {
-    _id: ID!
-    name: String!
-    email: String!
-    password: String!
-    picture: String
-    date_joined: String!
-  }
-
-  type Venue {
-    location_name: String!
-    address: String!
-    up_votes: Int
-    drink_name: String
-  }
-  type Auth{
-    token:ID!
-    user:User
+ 
+type User{
+name:String!
+email:String!
+password:String!
+bio:String
+date_joined:String
 }
-  type Query {
-    users: [User]!
-    venues:[Venue]!
-    user(userid:ID!):User
 
 
-    
-  }
 
-  type Mutation {
-    addUser(name: String!, email: String!, password: String!,pic:String,bio:String): Auth
-    login(email: String!, password: String!): Auth
-    removeUser: User
-
-  }
 `;
 
 module.exports = typeDefs;
