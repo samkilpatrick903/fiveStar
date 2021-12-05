7ad0fc592edf007c59fcbb35feb469c764f1eed8
brooke <brookejones406@gmail.com>
2021-12-05 12:49:51 -0600

    source

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,39 @@
 // import Customs from './testation/Customs'
-// import {Home} from './pages/home'
-
-import {Login} from './pages/Login'
-import {SignUp} from './pages/Signup'
+import Home from './pages/Home'
+import { Login } from "./pages/Login";
+import { SignUp } from "./pages/Signup";
 // import  {NavBar}  from './components/NavBar';
 // import  {FeaturedPost}from './components/booty'
 // import Row from './components/CoinInfo'
 // import {Coins} from './components/Coins'
 
-import React from 'react';
+import React from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it existsauthLink.concat(httpLink),
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -39,40 +43,30 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-
-
 function App() {
   return (
-
-      <ApolloProvider client={client}>
-        <Router>
-          <>
-         
-{/*           
-            <Switch>
-              <Route exact path='/' component={Home}/>
-            </Switch> */}
-            
-              <Route exact path='/login'component={Login} />
-              <Switch>
-              <Route exact path='/signup' component={SignUp}/>
-            </Switch>
-            {/* <Switch>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+          <Route exact path="/login" component={Login} />
+          <Switch>
+            <Route exact path="/signup" component={SignUp} />
+          </Switch>
+          {/* <Switch>
               <Route exact path='/coininfo' component={Row}/>
             </Switch> */}
-            {/* <Switch>
+          {/* <Switch>
               <Route exact path='/coins' component={Coins}/>
             </Switch>
             <Switch>
               <Route exact path='/featured' component={FeaturedPost}/>
             </Switch>  */}
-          </>
-        </Router>
-        
-       
-     </ApolloProvider>
-    
-
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
