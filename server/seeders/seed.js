03cfc2277d69978a85150b7a8c13583e5e785ceb
brooke <brookejones406@gmail.com>
2021-12-05 14:15:20 -0600

    seeds be seedin

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,5 +1,7 @@
+const locationData = require("./venueData.json");
 const db = require('../configs/connection');
 const  User  = require('../models/User');
+const  Venue  = require('../models/Venue');
 const userSeeds = require('./userSeeds.json');
 // const { default: axios } = require('axios');
 
@@ -7,6 +9,10 @@ db.once('open', async () => {
     await User.deleteMany({});
   
     await User.create(userSeeds);
+
+    await Venue.deleteMany({});
+  
+    await Venue.create(locationData);
   
     console.log('Technologies seeded!');
     process.exit(0);
