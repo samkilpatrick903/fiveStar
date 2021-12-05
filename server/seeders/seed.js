238952555cbc2bb4473fda7aeb34ddf722bf5c00
DerTodd <88950531+DerTodd@users.noreply.github.com>
2021-12-05 13:54:24 -0600

    seeds update.

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,12 @@
 const db = require('../configs/connection');
-const { User,Coin, Graph } = require('../models');
+const  User  = require('../models/User');
 const userSeeds = require('./userSeeds.json');
 // const { default: axios } = require('axios');
 
 db.once('open', async () => {
     await User.deleteMany({});
   
-    const technologies = await User.insertMany(userData);
+    await User.create(userSeeds);
   
     console.log('Technologies seeded!');
     process.exit(0);
