cbf95f196a6bbd9f1391f16a2e44057172d12959
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-12-06 23:36:21 -0600

    login and signup mended

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -69,7 +69,7 @@ const LoginForm = styled("form")({
 
 const theme = createTheme();
 
-export default function Login(props) {
+export default function Login() {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, {error,loading}] = useMutation(LOGIN_USER);
 
@@ -82,12 +82,11 @@ export default function Login(props) {
   }
   const handleLogin = async (event) => {
     event.preventDefault();
-    event.stopPropagation();
 
     console.log(formState);
     try {
       const { data } = await login({
-        variables: { formState },
+        variables: { ...formState },
       });
       if(loading) return "loading"
       Auth.login(data.login.token);
--- server/models/User.js
+++ server/models/User.js
@@ -29,9 +29,9 @@ const userSchema = new Schema({
 	},
 	password: {
 		type: String,
-		minLength: [ 6, 'You need a longer password' ],
+		//minLength: [ 6, 'You need a longer password' ],
 		required: 'You must provide a valid password',
-		trim: true
+		//trim: true
 	},
 	picture: {
 		type: String
@@ -48,29 +48,19 @@ const userSchema = new Schema({
 
 });
 
-userSchema.pre('save', function(next) {
-	var user = this;
-	// only hash the password if it has been modified (or is new)
-	if (!user.isModified('password')) return next();
-	// generate a salt
-	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-		if (err) return next(err);
-		// hash the password using our new salt
-		bcrypt.hash(user.password, salt, function(err, hash) {
-			if (err) return next(err);
-			// override the cleartext password with the hashed one
-			user.password = hash;
-			next();
-		});
-	});
-});
-
-userSchema.methods.isCorrectPassword = async function(candidatePassword, cb) {
-	await bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-		if (err) return cb(err);
-		cb(null, isMatch);
-	});
-};
+userSchema.pre('save', async function (next) {
+	if (this.isNew || this.isModified('password')) {
+	  const saltRounds = 10;
+	  this.password = await bcrypt.hash(this.password, saltRounds);
+	}
+  
+	next();
+  });
+  
+  // custom method to compare and validate password for logging in
+  userSchema.methods.isCorrectPassword = async function (password) {
+	return bcrypt.compare(password, this.password);
+  };
 
 const User = mongoose.model('User', userSchema);
 
